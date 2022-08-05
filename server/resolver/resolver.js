2400636733810e991fd3c2fdd23c4e20e34c84ab
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2022-08-06 02:28:57 +0700

    query and mutation

--- server/resolver/resolver.js
+++ server/resolver/resolver.js
@@ -1,17 +1,32 @@
+const { books, authors } = require('../data/static');
+
 const resolvers = {
+    // Query
     Query: {
-        books: () => [
-            {
-                id: 1,
-                name: 'vinh',
-                genre: 'Adventure',
-            },
-            {
-                id: 2,
-                name: 'Test',
-                genre: 'test',
-            },
-        ],
+        // define query
+        books: () => books,
+        book: (parent, args) => books.find(book => book.id == args.id),
+        authors: () => authors,
+        author: (parent, args) => authors.find(author => author.id == args.id),
+    },
+    Book: {
+        // when query have Book field
+        author: (parent, args) => {
+            // define when Book have author field
+            return authors.find(author => author.id == parent.authorId);
+        },
+    },
+    Author: {
+        books: (parent, args) => {
+            return books.filter(book => book.authorId == parent.id);
+        },
+    },
+    // Mutation
+    Mutation: {
+        createAuthor: (parent, args) => {
+            return args;
+        },
+        createBook: (parent, args) => args,
     },
 };
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,11 +5,28 @@ const typeDefs = gql`
         id: ID
         name: String
         genre: String
+        author: Author
     }
 
+    type Author {
+        id: ID!
+        name: String
+        age: Int
+        books: [Book]
+    }
+
+    # define type of query, left - name of query, right - result return
     #ROOT TYPE
     type Query {
         books: [Book]
+        book(id: ID!): Book
+        authors: [Author]
+        author(id: ID!): Author
+    }
+
+    type Mutation {
+        createAuthor(id: ID!, name: String, age: Int): Author
+        createBook(id: ID!, name: String, genre: String, authorId: ID!): Book
     }
 `;
 
